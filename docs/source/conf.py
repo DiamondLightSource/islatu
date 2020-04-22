56aac2e33e77980a410f40806fd2b638f0cb2f2b
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2020-04-22 16:25:03 +0100

    set version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2020, Andrew R. McCluskey'
 author = 'Andrew R. McCluskey'
 
 # The short X.Y version
-version = '0.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
