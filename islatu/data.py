037f71e885940fec73e71e7cf5d8086e826a44eb
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2021-09-23 14:03:50 +0100

    Refactored data.py to remove unumpy dependency

--- islatu/data.py
+++ islatu/data.py
@@ -18,11 +18,44 @@ data manipulation methods.
 
 import numpy as np
 from scipy.constants import physical_constants
-from uncertainties import unumpy as unp
 
 
 class Data:
-    def __init__(self, intensity, energy, theta=None, q=None):
+    def __init__(self, intensity, intensity_e, energy, theta=None, q=None):
+        """
+        The base class of all Islatu objects that contain data.
+
+        Attributes:
+            intensity:
+                A numpy array containing intensities in this dataset.
+            intensity_e:
+                A numpy array containing the corresponding errors in intensity.
+            theta:
+                A numpy array containing the probe particle's angle of 
+                incidence at each intensity.
+            q:
+                A numpy array containing the magnitude of the probe particle's
+                scattering vector for each intensity value.
+            energy:
+                The energy of the probe particle used to acquire this data. This
+                is necessary to swap between theta and q.
+
+        Args:
+            intensity:
+                A numpy array of the intensities in this dataset.
+            intensity_e:
+                The errors on the intensities.
+            energy:
+                The energy of the probe particle used to acquire this data.
+            theta:
+                A numpy array containing the probe particle's angle of 
+                incidence at each intensity. NOTE: only one of theta/q needs to
+                be provided.
+            q:
+                A numpy array containing the magnitude of the probe particle's
+                scattering vector for each intensity value. NOTE: only one of 
+                theta/q needs to be provided.
+        """
         self.intensity = intensity
         self.energy = energy
 
@@ -36,15 +69,22 @@ class Data:
         self._q = q
 
     @property
-    def R(self) -> unp.uarray:
-        return self.intensity
+    def R(self) -> np.array:
+        """
+        Returns the intensity, normalized such that the maximum value of the 
+        intensity is equal to 1. To acquire
+        """
+        return self.intensity/np.amax(self.intensity)
 
-    @R.setter
-    def R(self, value) -> None:
-        self.intensity = value
+    def R_e(self) -> np.array:
+        """
+        Returns the errors on the intensity, divided by the maximum value of the
+        intensity array.
+        """
+        return self.intensity_e/np.amax(self.intensity)
 
     @property
-    def q(self) -> unp.uarray:
+    def q(self) -> np.array:
         if (self._q is None) and (self._theta is not None):
             return self._theta_to_q(self._theta, self.energy)
         else:
@@ -55,7 +95,7 @@ class Data:
         self._q = value
 
     @property
-    def theta(self) -> unp.uarray:
+    def theta(self) -> np.array:
         if (self._theta is None) and (self._q is not None):
             return self._q_to_theta(self._q, self.energy)
         else:
@@ -65,7 +105,7 @@ class Data:
     def theta(self, value) -> None:
         self._theta = value
 
-    def _theta_to_q(self, theta, energy) -> unp.uarray:
+    def _theta_to_q(self, theta, energy) -> np.array:
         """
         Calculates the scattering vector Q from diffractometer theta.
 
@@ -78,12 +118,12 @@ class Data:
         planck = physical_constants["Planck constant in eV s"][0] * 1e-3
         speed_of_light = physical_constants[
             "speed of light in vacuum"][0] * 1e10
-        q_values = unp.sin(unp.radians(theta)) / (planck * speed_of_light)
+        q_values = np.sin(np.radians(theta)) / (planck * speed_of_light)
 
         q_values *= energy * 4.0 * np.pi
         return q_values
 
-    def _q_to_theta(self, q_values, energy) -> unp.uarray:
+    def _q_to_theta(self, q_values, energy) -> np.array:
         """
         Calculates the diffractometer theta from scattering vector Q.
 
@@ -97,7 +137,7 @@ class Data:
         speed_of_light = physical_constants[
             "speed of light in vacuum"][0] * 1e10
         theta_values = planck * speed_of_light * \
-            unp.arcsin(q_values / (energy * 4 * np.pi))
+            np.arcsin(q_values / (energy * 4 * np.pi))
 
         theta_values = theta_values*180/np.pi
 
@@ -111,7 +151,7 @@ class MeasurementBase:
     # Define some properties to make accessing things in self.data a bit cleaner
     # and more intuitive.
     @property
-    def q(self) -> unp.uarray:
+    def q(self) -> np.array:
         return self.data.q
 
     @q.setter
@@ -119,7 +159,7 @@ class MeasurementBase:
         self.data.q = value
 
     @property
-    def theta(self) -> unp.uarray:
+    def theta(self) -> np.array:
         return self.data.theta
 
     @theta.setter
