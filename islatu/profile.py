ad445c0360ed1af613d4db901851c4d5a0af44d0
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2021-09-24 13:46:47 +0100

    Corrected self.R to self.intensity in some places

--- islatu/profile.py
+++ islatu/profile.py
@@ -171,7 +171,7 @@ class Profile(MeasurementBase):
         """
         Class method for :func:`~islatu.stitching.concatenate`.
         """
-        self.q, self.R = stitching.concatenate(
+        self.q, self.intensity = stitching.concatenate(
             self.scans)
 
     def normalise_ter(self, max_q=0.1):
@@ -183,7 +183,7 @@ class Profile(MeasurementBase):
                 the critical angle.
         """
         self.reflected_intensity = stitching.normalise_ter(
-            self.q, self.R, max_q
+            self.q, self.intensity, max_q
         )
         self.concatenate()
 
@@ -198,7 +198,7 @@ class Profile(MeasurementBase):
                 q-vectors to be using initially in the rebinning of the data.
                 Defaults to :py:attr:`400`.
         """
-        self.q, self.R = stitching.rebin(
-            self.q, self.R, new_q,
+        self.q, self.intensity = stitching.rebin(
+            self.q, self.intensity, new_q,
             number_of_q_vectors, interpolate
         )
