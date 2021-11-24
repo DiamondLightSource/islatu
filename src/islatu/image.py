a0ec12f2ce1b4d3746b81ae0af6a82bf46c82767
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2021-11-24 15:43:43 +0000

    Simplified Image class

--- src/islatu/image.py
+++ src/islatu/image.py
@@ -1,18 +1,11 @@
 """
 The two-dimension detector generates images of the reflected intensity.
-The purpose of this class is the investigation and manipulation of these
-images.
+The purpose of the Image class stored in this module is the investigation
+and manipulation of these images.
 """
 
-# Copyright (c) 2021 Richard Brearton
-# Copyright (c) Andrew R. McCluskey
-# Distributed under the terms of the MIT License
-# authors: Richard Brearton and Andrew R. McCluskey
 
 import numpy as np
-from PIL import Image as PILIm
-
-from islatu.background import fit_gaussian_1d
 
 
 class Image:
@@ -32,9 +25,6 @@ class Image:
             The background that was subtracted from the image.
         bkg_e (:py:attr:`float`):
             The uncertainty on the background.
-        n_pixels (:py:attr:`float`):
-            The width of the peak in number of pixels, used to calculate an 
-            uncertainty in q on the detector.
 
     Args:
         file_path (:py:attr:`str`): The file path for the image.
@@ -42,39 +32,20 @@ class Image:
             the measurement. Defaults to :py:attr:`None`.
         transpose (:py:attr:`bool`, optional): Should the data be rotated by
             90 degrees? Defaults to :py:attr:`False`.
-        pixel_min (:py:attr:`int`): The minimum pixel value possible, all
-            pixels found with less than this value will have this value
-            assigned. Defaults to :py:attr:`0`.
-        hot_pixel_max (:py:attr:`int`): The value of a hot pixel that will be
-            set to the average of the surrounding pixels. Logically, this
-            should be less than the :py:attr:`pixel_max` value. Defaults to
-            :py:attr:`2e5`.
     """
 
-    def __init__(self, array, transpose=False, pixel_min=0,
-                 hot_pixel_max=2e5):
+    def __init__(self, array: np.ndarray, transpose: bool = False):
         """
         Initialisation of the :py:class:`islatu.image.Image` class, includes
         assigning uncertainties.
         """
         if transpose:
             array = array.T
-        array = _average_out_hot(array, hot_pixel_max)
-        array[np.where(array < pixel_min)] = 0
         self.array = array
         self.array_original = np.copy(array)
         self.array_e = self.initial_std_devs
         self.bkg = 0
         self.bkg_e = 0
-        self.n_pixels = 0
-
-    @classmethod
-    def from_img_file_name(cls, file_path, transpose=False, pixel_min=0,
-                           hot_pixel_max=2e5):
-        img = PILIm.open(file_path)
-        array = np.array(img)
-        img.close()
-        return cls(array, file_path, transpose, pixel_min, hot_pixel_max)
 
     @property
     def nominal_values(self):
@@ -97,26 +68,6 @@ class Image:
         array_error = np.sqrt(self.array_original)
         array_error[np.where(self.array_original == 0)] = 1
         return array_error
-
-    @property
-    def n(self):
-        """
-        Get the nominal values of the image array.
-
-        Returns:
-            :py:attr:`array_like`: Nominal values of image.
-        """
-        return self.array
-
-    @property
-    def s(self):
-        """
-        Get the standard deviation values of the image array.
-
-        Returns:
-            :py:attr:`array_like`: Standard deviation values of image.
-        """
-        return self.std_devs
 
     @property
     def shape(self):
@@ -168,105 +119,29 @@ class Image:
             **kwargs (:py:attr:`dict`): The background substraction function
                 keyword arguments.
         """
-        if background_subtraction_function is None:
-            # By default, don't do any fancy fitting - instead just subtract the
-            # mean count of a specified background region.
-            y_start = kwargs['y_start']
-            y_end = kwargs['y_end']
-            x_start = kwargs['x_start']
-            x_end = kwargs['x_end']
 
-            # Calculate Poisson error as though this whole region was just one
-            # big pixel called bkg_area
-            sum_of_bkg_area = np.sum(
-                self.array_original[y_start:y_end, x_start:x_end])
-            err_of_bkg_area = np.sqrt(sum_of_bkg_area)
-            num_pixels = (y_end - y_start) * (x_end - x_start)
+        bkg_sub_info = background_subtraction_function(
+            self, **kwargs
+        )
+        # Store the calculated background, and its error.
+        self.bkg, self.bkg_e = bkg_sub_info.bkg, bkg_sub_info.bkg_e
 
-            # Weird name so that we match names in the else statement.
-            bkg_popt = sum_of_bkg_area/num_pixels
-            bkg_sigma = err_of_bkg_area/num_pixels
-
-            self.bkg = bkg_popt
-            self.bkg_e = bkg_sigma
-        else:
-            bkg_popt, bkg_idx = background_subtraction_function(
-                self.n, self.s, **kwargs
-            )
-            self.bkg, self.bkg_e = bkg_popt[0][bkg_idx], bkg_popt[1][bkg_idx]
-
-        self.array = np.float64(self.array) - self.bkg
+        # Do the subtraction.
+        self.array = self.array - self.bkg
         self.array_e = np.sqrt(self.bkg_e**2 + self.array_e**2)
 
-        # Expose the optimized fit parameters for meta-analysis.
-        return bkg_popt
+        # Expose information relating to the background subtraction for
+        # meta-analyses.
+        return bkg_sub_info
 
-    def sum(self, axis=None):
+    def sum(self):
         """
-        Perform a summation on the image
+        Perform a summation on the image's array.
 
-        Args:
-            axis (:py:attr:`int`, optional): The axis of the array to perform
-                the summation over. Defaults to :py:attr:`None`.
+        Returns:
+            A tuple taking the form (summed_intensity, summed_intensity_e).
         """
-        intensity = self.array.sum(axis)
-
-        intensity_e_sq = self.array_e ** 2
-
-        intensity_e = np.sqrt(np.sum(intensity_e_sq))
+        intensity = np.sum(self.array)
+        intensity_e = np.sqrt(np.sum(self.array_e**2))
 
         return intensity, intensity_e
-
-    def q_resolution(self, qz_dimension=1):
-        """
-        Estimate the q-resolution function based on the reflected intensity
-        on the detector.
-
-        Args:
-            qz_dimension (:py:attr:`int`, optional): The dimension of q_z in
-                the detector image (this should be the opposite index to that
-                the summation is performed if the
-                :py:func:`islatu.background.fit_gaussian_1d` background
-                subtraction has been performed). Defaults to :py:attr:`1`.
-        """
-        bkg_popt, bkg_idx = fit_gaussian_1d(
-            self.n, self.s, axis=qz_dimension
-        )
-        self.n_pixels = bkg_popt[0][1]
-
-
-def _average_out_hot(array, hot_pixel_max=2e5):
-    """
-    Make hot pixels equal to the local average.
-
-    Args:
-        array (:py:attr:`array_like`): The array to have hot pixels removed.
-        hot_pixel_max (:py:attr:`float`, optional): The definition of a hot
-            pixel.
-
-    Returns:
-       :py:attr:`array_like`: Image array with hot pixels removed.
-    """
-    x, y = np.where(array > hot_pixel_max)
-    for i in range(x.size):
-        a = x[i]
-        b = y[i]
-        j = 1
-        k = 1
-        if a == 0:
-            a = 1
-            j = 0
-        elif a == array.shape[0] - 1:
-            a = array.shape[0] - 1
-            j = 1
-        if b == 0:
-            b = 1
-            k = 0
-        elif b == array.shape[1] - 1:
-            b = array.shape[1] - 1
-            k = 1
-        local = np.copy(array[a-1:a+2, b-1:b+2])
-        local[j, k] = 0
-        if local.mean() < (array[x[i], y[i]]) / 100:
-            array[x[i], y[i]] = local.sum() / (local.size - 1)
-    return array
