47f9a49c133413b93328f8eea6ced0ad8251f8fa
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2021-11-24 15:40:50 +0000

    Refactored profile according to lower lvl refactor

--- src/islatu/refl_profile.py
+++ src/islatu/refl_profile.py
@@ -1,75 +1,71 @@
 """
 A profile is a measurement resulting from a scan, or a series of scans. Profiles
-are the central objects in the islatu library, containing the total reflected 
+are the central objects in the islatu library, containing the total reflected
 intensity as a function of scattering vector data.
 """
 
-from islatu import stitching
-from islatu.stitching import concatenate
-from islatu.data import Data, MeasurementBase
+from typing import List
+
+from .scan import Scan
+from .stitching import concatenate, rebin
+from .data import Data
 
 
-class Profile(MeasurementBase):
-    def __init__(self, data: Data, scans: list) -> None:
-        super().__init__(data)
+class Profile(Data):
+    """
+    The object that is used to store all information relating to a reflectivity
+    profile.
+    """
+
+    def __init__(self, data: Data, scans: List[Scan]) -> None:
+        super().__init__(data.intensity, data.intensity_e, data.energy,
+                         data.theta)
         self.scans = scans
 
     @classmethod
-    def fromfilenames(cls, filenames, parser, log_lvl=1, scan_axis_name=None):
+    def fromfilenames(cls, filenames, parser):
         """
         Instantiate a profile from a list of scan filenames.
 
         Args:
-            filenames (:py:attr:`list`): 
+            filenames (:py:attr:`list`):
                 List of files, one for each reflectometry scan. Can have length
                 one.
-            parser (:py:attr:`callable`): 
+            parser (:py:attr:`callable`):
                 Parser function for the reflectometry scan files.
-            scan_axis_name (:py:attr:`str`):
-                Name of the independent variable scanned, typically a theta- or 
-                q- axis. Specifying this parameter is not necessary for file 
-                formats such as .nxs for which the independent variable can be
-                inferred.
         """
 
         # Load the scans, specifying the scan axis name if necessary.
-        if scan_axis_name is not None:
-            scans = [parser(filename, scan_axis_name, log_lvl)
-                     for filename in filenames]
-        else:
-            scans = [parser(filename, log_lvl) for filename in filenames]
+        scans = [parser(filename) for filename in filenames]
 
         # Now that the individual scans have been loaded, data needs to be
         # constructed. The simplest way to do this is by concatenating the
         # data from each of the constituent scans.
-        q, intensity, intensity_e = concatenate(scans)
+        q_vectors, intensity, intensity_e = concatenate(scans)
 
         # Note: we are making the implicit assumption that energy is independent
         # of scan number at this point.
         energy = scans[0].metadata.probe_energy
 
-        data = Data(intensity, intensity_e, energy, q=q)
+        data = Data(intensity, intensity_e, energy, q_vectors=q_vectors)
 
         return cls(data, scans)
 
-    def crop(self, crop_function, kwargs=None, progress=True):
+    def crop(self, crop_function, **kwargs):
         """
-        Calls the Class method for the :func:`~islatu.scan.Scan2D.crop` 
+        Calls the Class method for the :func:`~islatu.scan.Scan2D.crop`
         method for each :py:class:`~Scan2D` in :py:attr:`self.scans`.
 
         Args:
             crop_function (:py:attr:`callable`): Cropping function to be used.
             kwargs (:py:attr:`dict`, optional): Keyword arguments for the
                 cropping function. Defaults to :py:attr:`None`.
-            progress (:py:attr:`bool`, optional): Show a progress bar.
-                Requires the :py:mod:`tqdm` package. Defaults to
-                :py:attr:`True`.
         """
         for scan in self.scans:
-            scan.crop(crop_function, kwargs, progress)
+            scan.crop(crop_function, **kwargs)
         self.concatenate()
 
-    def bkg_sub(self, bkg_sub_function, kwargs=None, progress=True):
+    def bkg_sub(self, bkg_sub_function, **kwargs):
         """
         Class method for the :func:`~islatu.refl_data.Scan.bkg_sub` method for
         each :py:class:`~Scan` in the list.
@@ -80,9 +76,6 @@ class Profile(MeasurementBase):
             kwargs (:py:attr:`dict`, optional): Keyword arguments for
                 the background subtraction function. Defaults to
                 :py:attr:`None`.
-            progress (:py:attr:`bool`, optional): Show a progress bar.
-                Requires the :py:mod:`tqdm` package. Defaults to
-                :py:attr:`True`.
         """
         # When a scan subtracts background from each of its images, its
         # background subtraction function may expose information relating to the
@@ -92,22 +85,22 @@ class Profile(MeasurementBase):
         # background, storing the return values in bkg_sub_info.
         for scan in self.scans:
             bkg_sub_info.append(scan.bkg_sub(
-                bkg_sub_function, kwargs, progress))
+                bkg_sub_function, **kwargs))
 
         self.concatenate()
 
         # Expose the optimized fit parameters for meta-analysis.
         return bkg_sub_info
 
-    def subsample_q(self, scan_ID, q_min=0, q_max=float('inf')):
+    def subsample_q(self, scan_identifier, q_min=0, q_max=float('inf')):
         """
-        For the scan scan_ID, delete all data points for which q < q_min or
-        q > q_max.
+        For the scan with identifier scan_identifier, delete all data points for
+        which q < q_min or q > q_max.
 
         Args:
-            scan_ID:
+            scan_identifier:
                 The scan ID of the scan to be subsampled. This must be a unique
-                substring of the filename from which the scan was taken. For 
+                substring of the filename from which the scan was taken. For
                 example, if a scan's nexus filename is i07-413244.nxs, then
                 a valid scan_ID would be "413244", as this string will uniquely
                 identify the correct scan from within the profile.
@@ -117,7 +110,8 @@ class Profile(MeasurementBase):
                 The largest acceptable value of q. Defaults to inf Å.
         """
         for scan in self.scans:
-            if scan_ID in scan.metadata.filename:
+            print(scan_identifier, scan.metadata.src_path)
+            if scan_identifier in scan.metadata.src_path:
                 scan.subsample_q(q_min, q_max)
         self.concatenate()
 
@@ -136,45 +130,13 @@ class Profile(MeasurementBase):
             scan.footprint_correction(beam_width, sample_size)
         self.concatenate()
 
-    def transmission_normalisation(self, allow_stitching=False):
+    def transmission_normalisation(self):
         """
-        Perform the transmission correction
-        (:func:`~islatu.refl_data.Scan.transmission_normalisation`) for each
-        :py:class:`~Scan` in the list and fine attenutation correction
-        (:func:`~islatu.stitching.correction_attenutation`).
+        Perform the transmission correction.
         """
         for scan in self.scans:
             scan.transmission_normalisation()
 
-        if allow_stitching:
-            self.scans = stitching.correct_attentuation(
-                self.scans, allow_stitching)
-        self.concatenate()
-
-    def resolution_function(self, qz_dimension=1, progress=True,
-                            pixel_size=172e-6):
-        """
-        Class method for
-        :func:`~islatu.refl_data.Scan.q_uncertainty_from_pixel` for each
-        :py:class:`~Scan` in the list.
-
-       Args:
-           qz_dimension (:py:attr:`int`, optional): The dimension of q_z in
-               the detector image (this should be the opposite index to that
-               the summation is performed if the
-               :py:func:`islatu.background.fit_gaussian_1d` background
-               subtraction has been performed). Defaults to :py:attr:`1`.
-            number_of_pixels (:py:attr:`float`): Number of pixels of q
-                uncertainty.
-            detector_distance (:py:attr:`float`): Sample detector distance in
-                metres
-            energy (:py:attr:`float`): X-ray energy in keV
-            pixel_size (:py:attr:`float`, optional): Pixel size in metres
-        """
-        for scan in self.scans:
-            scan.resolution_function(
-                qz_dimension, progress, pixel_size
-            )
         self.concatenate()
 
     def qdcd_normalisation(self, itp):
@@ -194,39 +156,26 @@ class Profile(MeasurementBase):
         """
         Class method for :func:`~islatu.stitching.concatenate`.
         """
-        self.q, self.intensity, self.intensity_e = stitching.concatenate(
-            self.scans)
-
-    def normalise_ter(self, max_q=0.1):
-        """
-        Class method for :func:`~islatu.stitching.normalise_ter`.
-
-        Args:
-            max_q (:py:attr:`float`): The maximum q to be included in finding
-                the critical angle.
-        """
-        self.reflected_intensity = stitching.normalise_ter(
-            self.q, self.intensity, max_q
-        )
-        self.concatenate()
+        self.q_vectors, self.intensity, self.intensity_e = \
+            concatenate(self.scans)
 
     def rebin(self, new_q=None, rebin_as="linear", number_of_q_vectors=5000):
         """
         Class method for :func:`islatu.stitching.rebin`.
 
         Args:
-            new_q (:py:attr:`array_like`): 
-                Array of potential q-values. Defaults to :py:attr:`None`. If 
-                this argument is not specified, then the new q, R values are 
+            new_q (:py:attr:`array_like`):
+                Array of potential q-values. Defaults to :py:attr:`None`. If
+                this argument is not specified, then the new q, R values are
                 binned according to rebin_as and number_of_q_vectors.
             rebin_as (py:attr:`str`):
-                String specifying how the data should be rebinned. Options are 
-                "linear" and "log". This is only used if the new_q are 
+                String specifying how the data should be rebinned. Options are
+                "linear" and "log". This is only used if the new_q are
                 unspecified.
             number_of_q_vectors (:py:attr:`int`, optional):
-                The max number of q-vectors to be using initially in the 
+                The max number of q-vectors to be using initially in the
                 rebinning of the data. Defaults to :py:attr:`400`.
         """
-        self.q, self.intensity, self.intensity_e = stitching.rebin(
-            self.q, (self.intensity, self.intensity_e), new_q,
+        self.q_vectors, self.intensity, self.intensity_e = rebin(
+            self.q_vectors, (self.intensity, self.intensity_e), new_q,
             rebin_as=rebin_as, number_of_q_vectors=number_of_q_vectors)
