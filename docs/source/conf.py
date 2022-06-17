588ebaa1e410ffc0c27a5eeeee79582b0033f010
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-06-17 14:02:20 +0100

    Updated version numbers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ project = 'islatu'
 copyright = '2020, Andrew R. McCluskey'
 author = 'Andrew R. McCluskey'
 
-version = '1.0.1'
+version = '1.0.2'
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ REQUIREMENTS = [
 
 setup(
     name='islatu',
-    version='1.0.0',
+    version='1.0.2',
     license='MIT',
     description='A package for the reduction of reflectometry data.',
     author='Richard Brearton',
