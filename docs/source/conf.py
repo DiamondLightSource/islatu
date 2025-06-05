5bc0a7aa2b1ad45f93af85790f7e2077606cba28
Philip Mousley <philipmousley@hotmail.com>
2025-06-05 12:17:02 +0100

    update documentation version label to 1.0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ project = 'islatu'
 copyright = '2020-2025, Diamond Light Source Ltd'
 author = 'Philip Mousley'
 
-version = '1.0.8'
+version = '1.0.9'
 # The full version, including alpha/beta/rc tags
-release = '1.0.8'
+release = '1.0.9'
 
 
 # -- General configuration ---------------------------------------------------
