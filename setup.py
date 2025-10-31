dbbc8ae22c3778549e4df71e09ec6110fe15cbdb
pmousley <philipmousley@hotmail.com>
2025-10-31 10:14:34 +0000

    edit version to 1.2.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ REQUIREMENTS = [
 
 setup(
     name='islatu',
-    version='1.1.0',
+    version='1.2.0',
     license='MIT',
     description='A package for the reduction of reflectometry data.',
     author='Philip Mousley',
