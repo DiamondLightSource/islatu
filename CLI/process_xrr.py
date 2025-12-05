bc736935e880411817457fffd3826741f49c4933
pmousley <philipmousley@hotmail.com>
2025-12-05 11:42:02 +0000

    edit parsing of script template for correct python version

--- CLI/process_xrr.py
+++ CLI/process_xrr.py
@@ -286,23 +286,23 @@ if __name__ == "__main__":
         f.close()
             
         #load in template mapscript, new paths
-        f=open(f'{version_path}/islatu/CLI/islatuscript_template.sh')
-        lines=f.readlines()
-        f.close()
+        with open(f'{version_path}/islatu/CLI/islatuscript_template.sh') as f:
+            lines=f.readlines()
+
         jobfile=f'{islatufolder}//jobscript.sh'
         if os.path.exists(jobfile):
             f=open(jobfile,'w')
         else:
             f=open(jobfile,'x')
         for line in lines:
-            if '$' in line:
-                phrase=line[line.find('$'):line.find('}')+1]
+            phrase_matches=list(re.finditer(r'\${[^}]+\}',line))
+            phrase_positions=[(match.start(),match.end()) for match in phrase_matches]
+            outline=line
+            for pos in phrase_positions:
+                phrase=line[pos[0]:pos[1]]
                 outphrase=phrase.strip('$').strip('{').strip('}')
-                outline=line.replace(phrase,str(locals()[f'{outphrase}']))
-                #print(outline)
-                f.write(outline)
-            else:
-                f.write(line)
+                outline=outline.replace(phrase,str(locals()[f'{outphrase}']))
+            f.write(outline)
         f.close()
         
         #get list of slurm out files in home directory
