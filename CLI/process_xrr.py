38a267fb34fc403ececb4c8d8b662aa92b7419d7
P-Mousley <philipmousley@hotmail.com>
2026-08-07 09:57:03 +0100

    ruff formatting

--- CLI/process_xrr.py
+++ CLI/process_xrr.py
@@ -3,17 +3,18 @@
 """Command line interface for the Islatu library."""
 
 import argparse
+
 from islatu.runner import ProcessArgs
 
-version_path=__file__.split('islatu/CLI',maxsplit=1)[0]
-python_version=version_path+'/conda_env/bin/python'
+version_path = __file__.split("islatu/CLI", maxsplit=1)[0]
+python_version = version_path + "/conda_env/bin/python"
 
 if __name__ == "__main__":
     # First deal with the parsing of the command line arguments using the
     # argparse library.
     HELP_STR = (
-        "Command line interface to the Islatu library's autoprocessing " +
-        "functionality."
+        "Command line interface to the Islatu library's autoprocessing "
+        + "functionality."
     )
     parser = argparse.ArgumentParser(description=HELP_STR)
 
@@ -24,56 +25,54 @@ if __name__ == "__main__":
     # simple hardcoded values. Instead, default values are calculated after
     # parse_args is called.
     HELP_STR = (
-        "Path to the directory in which the data is stored. If this " +
-        "is not specified, your current directory will be used."
+        "Path to the directory in which the data is stored. If this "
+        + "is not specified, your current directory will be used."
     )
     parser.add_argument("-d", "--data_path", help=HELP_STR)
 
     HELP_STR = (
-        "Path to the .yaml recipe file. " +
-        "If this is not specified, this module will search your data " +
-        "directory, and data_path/processing/, for a .yaml file."
+        "Path to the .yaml recipe file. "
+        + "If this is not specified, this module will search your data "
+        + "directory, and data_path/processing/, for a .yaml file."
     )
     parser.add_argument("-y", "--yaml_path", help=HELP_STR)
 
     HELP_STR = (
-        "Use this flag if you are on site in diamond and would like your " +
-        "data to be processed on a cluster."
+        "Use this flag if you are on site in diamond and would like your "
+        + "data to be processed on a cluster."
     )
     parser.add_argument("-c", "--cluster", help=HELP_STR, action="store_true")
 
     HELP_STR = (
-        "Specify the first scan number to process. If this is not specified, " +
-        "no lower bound on scan number will be placed on scans found in the " +
-        "data directory. If neither lower nor upper bounds are placed, all  " +
-        "scans found in the data directory will be used to construct a profile."
+        "Specify the first scan number to process. If this is not specified, "
+        + "no lower bound on scan number will be placed on scans found in the "
+        + "data directory. If neither lower nor upper bounds are placed, all  "
+        + "scans found in the data directory will be used to construct a profile."
     )
     parser.add_argument("-l", "--lower_bound", help=HELP_STR, type=int)
 
     HELP_STR = (
-        "Specify the final scan number to process. If this is not specified, " +
-        "no upper bound will be placed on scan number for scans found in the " +
-        "data directory."
+        "Specify the final scan number to process. If this is not specified, "
+        + "no upper bound will be placed on scan number for scans found in the "
+        + "data directory."
     )
     parser.add_argument("-u", "--upper_bound", help=HELP_STR, type=int)
 
     HELP_STR = (
-        "Directly specify the scan numbers to be used to construct the " +
-        "profile. Simply sequentially list the scan numbers. Example usage: " +
-        "python3 process_xrr.py --scan_numbers 401320 401321 401324 401326 " +
-        "-d data/ -o processed_curves/. This argument overwrites -l and -u."
+        "Directly specify the scan numbers to be used to construct the "
+        + "profile. Simply sequentially list the scan numbers. Example usage: "
+        + "python3 process_xrr.py --scan_numbers 401320 401321 401324 401326 "
+        + "-d data/ -o processed_curves/. This argument overwrites -l and -u."
     )
-    parser.add_argument("-N", "--scan_numbers",
-                        help=HELP_STR, nargs='*', type=int)
+    parser.add_argument("-N", "--scan_numbers", help=HELP_STR, nargs="*", type=int)
 
     HELP_STR = (
-        "Specify the directory in which you would like your processed " +
-        "reflectivity curve to be stored. Defaults to data_path/processing/"
+        "Specify the directory in which you would like your processed "
+        + "reflectivity curve to be stored. Defaults to data_path/processing/"
     )
     parser.add_argument("-o", "--output", help=HELP_STR)
 
-    HELP_STR = (
-        """
+    HELP_STR = """
         Specify a list of scans whose q values should be limited, as well as the
         corresponding acceptable minimum and maximum q-values. For example:
             -Q 413243 0 0.4 413244 0.3 0.6 413248 0.8 inf
@@ -87,13 +86,9 @@ if __name__ == "__main__":
 import sys
 print(sys.executable)d.
         """
-    )
-    parser.add_argument("-Q", "--limit_q",
-                        help=HELP_STR, nargs='*', type=str)
+    parser.add_argument("-Q", "--limit_q", help=HELP_STR, nargs="*", type=str)
 
-    HELP_STR = (
-        "Specify a list of "
-    )
+    HELP_STR = "Specify a list of "
 
     # A switch to allow verbosity toggle.
     HELP_STR = "Increase output verbosity. -v = verbose, -vv = very verbose!"
@@ -101,11 +96,11 @@ print(sys.executable)d.
 
     # Extract the arguments from the parser.
     args = parser.parse_args()
-    args.version_path=version_path
-    args.python_version=python_version
+    args.version_path = version_path
+    args.python_version = python_version
 
-    args.jobfile_template=f'{version_path}/islatu/CLI/islatuscript_template.sh'
-    args.jobfile_name='jobscript_local.sh'
+    args.jobfile_template = f"{version_path}/islatu/CLI/islatuscript_template.sh"
+    args.jobfile_name = "jobscript_local.sh"
 
     process_args = ProcessArgs(**vars(args))
     process_args.parse_and_reduce()
--- src/islatu/runner.py
+++ src/islatu/runner.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from os import path
 from pathlib import Path
 from types import SimpleNamespace
-from typing import List
 
 try:
     from yaml import CLoader as Loader
@@ -23,10 +22,7 @@ import numpy as np
 from yaml import dump, load
 
 import islatu
-import islatu.background as background
-import islatu.corrections as corrections
-import islatu.cropping as cropping
-import islatu.io as io
+from islatu import background, corrections, cropping, io
 from islatu.config_loader import check_config_schema
 from islatu.debug import debug
 from islatu.io import i07_dat_to_dict_dataframe
@@ -78,7 +74,7 @@ class Measurement:
     """
 
     scheme: str = "q-dispersive"
-    q_range: List[str] = (str(-np.inf), str(np.inf))
+    q_range: list[str] = (str(-np.inf), str(np.inf))
     theta_axis_name: str = "dcdtheta"
     q_axis_name: str = "qdcd"
     transpose: bool = False
@@ -660,9 +656,7 @@ class Foreperson:
                 except TypeError:
                     pass
             else:
-                raise ValueError(
-                    "No sample size given in setup of {}.".format(self.yaml_file)
-                )
+                raise ValueError(f"No sample size given in setup of {self.yaml_file}.")
             if "beam width" in recipe["setup"].keys():
                 self.reduction.beam_width = make_tuple(recipe["setup"]["beam width"])
                 try:
@@ -810,7 +804,7 @@ def i07reduce(
             )
             return
         the_boss.reduction.crop_kwargs = {"region": roi}
-        debug.log(f"Crop ROI '{str(roi)}' generated from the .nxs file.")
+        debug.log(f"Crop ROI '{roi!s}' generated from the .nxs file.")
     elif "x_end" in the_boss.reduction.crop_kwargs:
         the_boss.reduction.crop_kwargs = {
             "region": Region(**the_boss.reduction.crop_kwargs)
@@ -928,9 +922,7 @@ def i07reduce(
     # Work out where to save the file.
     datetime_str = datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
     run_numbers.sort()
-    dat_filename = (
-        "XRR_{}_".format(run_numbers[0]) + yaml_pipeline_name + datetime_str + ".dat"
-    )
+    dat_filename = f"XRR_{run_numbers[0]}_" + yaml_pipeline_name + datetime_str + ".dat"
     if filename is None:
         # Make sure that the processing directory exists.
         processing_path = path.join(the_boss.directory_path, "processing")
@@ -1019,7 +1011,7 @@ def i07reduce_noload(
             )
             return
         the_boss.reduction.crop_kwargs = {"region": roi}
-        debug.log(f"Crop ROI '{str(roi)}' generated from the .nxs file.")
+        debug.log(f"Crop ROI '{roi!s}' generated from the .nxs file.")
     elif "x_end" in the_boss.reduction.crop_kwargs:
         the_boss.reduction.crop_kwargs = {
             "region": Region(**the_boss.reduction.crop_kwargs)
@@ -1140,9 +1132,7 @@ def i07reduce_noload(
     # Work out where to save the file.
     datetime_str = datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
     run_numbers.sort()
-    dat_filename = (
-        "XRR_{}_".format(run_numbers[0]) + yaml_pipeline_name + datetime_str + ".dat"
-    )
+    dat_filename = f"XRR_{run_numbers[0]}_" + yaml_pipeline_name + datetime_str + ".dat"
     if filename is None:
         # Make sure that the processing directory exists.
         processing_path = path.join(the_boss.directory_path, "processing")
